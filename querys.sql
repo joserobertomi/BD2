15898b0f4821b824c725ee9f56c96bc2374bb06b
Jose Roberto <joserobertomi@outlook.com>
2025-04-24 11:39:40 -0300

    init repo

---
+++ querys.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS contato(
+	id_contanto SERIAL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	empresa VARCHAR(30) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	phone VARCHAR(30) NULL,
+	cargo VARCHAR(10) NOT NULL
+);
+
+ALTER TABLE contato RENAME id_contanto TO id_contato;
+ALTER TABLE contato DROP column phone;
+
+SELECT * FROM contato ORDER BY first_name;
+
+CREATE TABLE IF NO EXISTS(
+	id_contato INT NOT NULL
+	phone CHARACTER(9) PRIMARY KEY,
+	dd CHARACTER(2) NOT NULL,
+	type_phone VARCHAR(50) NOT NULL,
+	FOREING KEY
+)
